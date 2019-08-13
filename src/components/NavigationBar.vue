ffe6ed37c96d13d4ed03f3e0321d495b0f31ba84
manyuet <907878374@qq.com>
2019-08-13 15:49:27 +0800

    网站顶部导航栏

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,69 @@
+<template>
+    <el-menu class="el-menu-demo" text-color="#000000" active-text-color="#FC6720" mode="horizontal">
+        <el-submenu index="1" >
+            <template slot="title" >小米手机</template>
+            <el-menu-item index="1-1" mode="horizontal">小米CC9</el-menu-item>
+            <el-menu-item index="1-2" mode="horizontal">小米CC9e</el-menu-item>
+            <el-menu-item index="1-3" mode="horizontal">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+            <template slot="title">Redmi 红米</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+            <template slot="title">电视</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+            <el-menu-item index="3-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="4">
+            <template slot="title">笔记本</template>
+            <el-menu-item index="4-1">选项1</el-menu-item>
+            <el-menu-item index="4-2">选项2</el-menu-item>
+            <el-menu-item index="4-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="5">
+            <template slot="title">家电</template>
+            <el-menu-item index="5-1">选项1</el-menu-item>
+            <el-menu-item index="5-2">选项2</el-menu-item>
+            <el-menu-item index="5-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="6">
+            <template slot="title">路由器</template>
+            <el-menu-item index="6-1">选项1</el-menu-item>
+            <el-menu-item index="6-2">选项2</el-menu-item>
+            <el-menu-item index="6-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-submenu index="7">
+            <template slot="title">智能硬件</template>
+            <el-menu-item index="7-1">选项1</el-menu-item>
+            <el-menu-item index="7-2">选项2</el-menu-item>
+            <el-menu-item index="7-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="8"><a href="https://www.ele.me" target="_blank">服务</a></el-menu-item>
+        <el-menu-item index="9"><a href="https://www.ele.me" target="_blank">社区</a></el-menu-item>
+        <el-menu-item index="10">
+            <el-input v-model="input" placeholder="请输入内容">
+                <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+    export default {
+        name: 'NavigationBar',
+        data: function () {
+            return {
+                input: ''
+            }
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
